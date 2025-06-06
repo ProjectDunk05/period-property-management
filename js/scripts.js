5ceea83fa98400aba55567013e3acb8183e916a3
Adam Green <green.adam10@sky.com>
2025-06-06 18:23:26 +0100

    add functions to create and control slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,44 @@
+// toggle function for hambuger menu display
 function toggleMenu() {
     document.getElementById('nav-menu').classList.toggle('active');
   }
+
+ // Image slider 
+const slides = document.querySelectorAll(".slides img"); 
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+
+  if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 7500);
+    }
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
